76112d507284be463aad13f76cd893f4762bfde0
AledOscourt <julien.fournier2508@gmail.com>
2022-01-17 13:59:46 +0100

    Accueil V1

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,39 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Salon du vin</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
     <link rel="stylesheet" href="assets/css/style.css" />
 </head>
+
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-danger bg-gradient border-bottom border-2 border-dark fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Accueil</a>
+            <div class="text-end">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="programme.php">Programme</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Liste d'exposants</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+                <div class="d-flex justify-content-center">
+                    <a class="btn btn-outline-dark">Inscription</a>
+                </div>
+            </div>
+        </div>
+    </nav>
--- index.php
+++ index.php
@@ -1,5 +1,113 @@
 <?php require_once 'includes/header.php' ?>
 
+<header></header>
 
+<div class="container-fluid bg-danger bg-gradient py-2">
+    <div class="row py-1 text-md-start text-center">
+        <h2>Présentation du thème</h2>
+    </div>
+    <div class="row">
+        <p>Après cette crise sanitaire qui nous a tous impactés, nous sommes à nouveau sur de belles perspectives. Les vignerons et les équipes du Salon n’attendent qu’une chose : retrouver ce lien et ces moments de joie et de convivialité qui nous sont chers ! Nous avons donc le plaisir de vous communiquer les dates des prochains salons du vin qui se tiendront sur le second semestre 2021 et le premier semestre 2022.</p>
+    </div>
+    <div class="row justify-content-center">
+        <a href="inscription.php" class="btn btn-outline-dark col-lg-4 col-md-6 col-sm-8 col-10">Inscription</a>
+    </div>
+</div>
 
+<div class="p-md-5 p-sm-4 bg-danger bg-gradient">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" data-bs-touch="true">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="assets/img/2016-10-17-salon-vinomedia-villeurbanne006.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="assets/img/2016-10-17-salon-vinomedia-villeurbanne028.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="assets/img/2016-10-17-salon-vinomedia-villeurbanne123.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <div class="d-md-flex d-none">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</div>
+<div class="container-fluid py-3 px-5 bg-danger bg-gradient d-grid gap-3">
+    <div class="row gap-md-4 gap-3">
+        <div class="card col-md">
+            <div class="card-body">
+                <h5 class="card-title">Nantes</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Hippodrome d’Enghien-les-Bains</h6>
+                <p class="card-text">95230</p>
+            </div>
+            <p class="position-absolute top-0   translate-middle badge rounded-pill bg-success bg-gradient">
+                DU 4 AU 6 FÉVRIER 2022
+            </p>
+        </div>
+        <div class="card col-md">
+            <div class="card-body">
+                <h5 class="card-title">Lyon</h5>
+                <h6 class="card-subtitle mb-2 text-muted">A l’Espace Champerret</h6>
+                <p class="card-text">6 Rue Jean Oestreicher, 75017</p>
+            </div>
+            <p class="position-absolute top-0   translate-middle badge rounded-pill bg-success bg-gradient">
+                DU 11 AU 13 MARS 2022
+            </p>
+        </div>
+        <div class="card col-md">
+            <div class="card-body">
+                <h5 class="card-title">Paris</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Parc des Expositions de Paris Versailles</h6>
+                <p class="card-text">1, place de la Porte de Versailles, 75015</p>
+            </div>
+            <p class="position-absolute top-0   translate-middle badge rounded-pill bg-success bg-gradient">
+                DU 4 AU 6 FÉVRIER 2022
+            </p>
+        </div>
+    </div>
+    <div class="row gap-md-5 gap-3">
+        <div class="card col-md">
+            <div class="card-body">
+                <h5 class="card-title">Villeurbane</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Espace Tête d’Or</h6>
+                <p class="card-text">103 Boulevard de Stalingrad, 69100</p>
+            </div>
+            <p class="position-absolute top-0  translate-middle badge rounded-pill bg-success bg-gradient">
+                DU 3 AU 5 DECEMBRE 2022
+            </p>
+        </div>
+        <div class="card col-md">
+            <div class="card-body">
+                <h5 class="card-title">Angers</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Parc des Expositions d’Angers</h6>
+                <p class="card-text">Route de Paris, 49044</p>
+            </div>
+            <p class="position-absolute top-0  translate-middle badge rounded-pill bg-success bg-gradient">
+            DU 1ER AU 3 AVRIL 2022
+            </p>
+        </div>
+        <div class="card col-md">
+            <div class="card-body">
+                <h5 class="card-title">Metz</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Parc des Expositions</h6>
+                <p class="card-text">Rue de la Grange aux Bois, 57000</p>
+            </div>
+            <p class="position-absolute top-0  translate-middle badge rounded-pill bg-success bg-gradient">
+            DU 22 AU 24 AVRIL 2022
+            </p>
+        </div>
+    </div>
+</div>
 <?php require_once 'includes/footer.php' ?>
